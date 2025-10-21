34df0d11e848f5dabb55af2f0d4eefa45c709e61
vzisiadis <vzisiadis@gmail.com>
2025-10-21 15:54:51 +0300

    updated skip eval condition

--- evaluations/evaluate.sh
+++ evaluations/evaluate.sh
@@ -37,16 +37,21 @@ pip install -r evaluations/requirements.txt
 export PYTHONPATH="$(pwd):$(pwd)/src"
 
 # 5) Generate eval-input
-echo "▶ Generating eval input…"
-python evaluations/generate_eval_input.py
-
-# 6) Conditionally run evaluation
 if [ "$SKIP_EVAL" = false ]; then
-  echo "▶ Running evaluation…"
-  python evaluations/evaluate.py
+  echo "▶ Generating eval input…"
+  python evaluations/generate_eval_input.py
 else
-  echo "▶ Skipping evaluation as requested (--skip-eval)."
+  echo "▶ Skipping generate evaluation as requested (--skip-eval)."
 fi
+
+# # 6) Conditionally run evaluation
+# if [ "$SKIP_EVAL" = false ]; then
+#   echo "▶ Running evaluation…"
+#   python evaluations/evaluate.py
+# else
+#   echo "▶ Skipping evaluation as requested (--skip-eval)."
+# fi
+python evaluations/evaluate.py
 
 # 7) Teardown
 deactivate
